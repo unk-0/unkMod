34793f5d397bc49adca66ae2ee31bacacb5ec9d4
alextomflykt <alexflykt@hotmail.se>
2025-03-05 03:53:10 +0100

    Stole gecko/hash/monkey balance changes

---
+++ common/defines/00_hashdiplodefines.lua
@@ -0,0 +1,14 @@
+--Diplomacy overrides
+--NDefines.NDiplomacy.
+NDefines.NDiplomacy.DISHONORABLE_PEACE_MONTHS=0
+NDefines.NDiplomacy.CRUSADE_TIMEOUT_YEARS = 10
+NDefines.NDiplomacy.ANNUL_TREATIES_YEARS = 20
+NDefines.NDiplomacy.SUPPORT_REBELS_COST = 60
+NDefines.NDiplomacy.PEACE_COST_ANNUL = 20 		
+NDefines.NDiplomacy.MIN_RELATIONS_TO_ALLY = -200					-- Alliances not possible if either country has an opinion of the other lower than this
+NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 12			-- Before this many months have passed in the war, you cannot gain 100% warscore by just occupying the warleader
+NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 10						-- The number of ordinary (i.e. non-leaving) Great Powers if RightsOfManDLC is enabled.
+NDefines.NDiplomacy.DISHONORABLE_PEACE_MONTHS = 0
+--Pending review
+--JUSTIFY_TRADE_CONFLICT_LIMIT = 0.4,			-- How big share of the trade power needed on the target to be able to justify a trade conflict
+--JUSTIFY_TRADE_CONFLICT_ACTOR_LIMIT = 0.2,		-- How big share of the trade power needed on the actor to be able to justify a trade conflict
